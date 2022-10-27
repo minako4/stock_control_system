379dacd80582fb541eed88f76eb8b4717eb4af98
minako.suzuki <sminako32@gmail.com>
2022-10-27 19:31:43 +0900

    ログインアクションの作成

--- src/main/java/actions/LoginAction.java
+++ src/main/java/actions/LoginAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.StoreView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.StoreService;
 
 
@@ -50,6 +53,49 @@ public class LoginAction extends ActionBase {
 
         //ログイン画面を表示
         forward(ForwardConst.FW_LOGIN);
+    }
+    /**
+     * ログイン処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login() throws ServletException, IOException {
+
+        String storeCode = getRequestParam(AttributeConst.STORE_STORE_CODE);
+        String plainPass = getRequestParam(AttributeConst.STORE_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        //有効な店舗か認証する
+        Boolean isValidStore = service.validateLogin(storeCode, plainPass, pepper);
+
+        if (isValidStore) {
+            //認証成功の場合
+
+            //CSRF対策 tokenのチェック
+            if (checkToken()) {
+
+                //ログインした店舗のDBデータを取得
+                StoreView sv = service.findOne(storeCode, plainPass, pepper);
+                //セッションにログインした店舗を設定
+                putSessionScope(AttributeConst.LOGIN_STORE, sv);
+                //セッションにログイン完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+                //トップページへリダイレクト
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        } else {
+            //認証失敗の場合
+
+            //CSRF対策用トークンを設定
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            //認証失敗エラーメッセージ表示フラグをたてる
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            //入力された店舗コードを設定
+            putRequestScope(AttributeConst.STORE_STORE_CODE, storeCode);
+
+            //ログイン画面を表示
+            forward(ForwardConst.FW_LOGIN);
+        }
     }
 
 }
